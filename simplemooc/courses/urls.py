d80965acfb5b663fbc649c7b99955c2cc5f228bd
Sergio Queiroz <queirozsc@gmail.com>
2020-04-04 12:09:05 -0300

    Initial commit

---
+++ simplemooc/courses/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<slug>', views.details, name='details'),
+    #re_path(r'^(?P<pk>\d+)/$', views.details, name='details'),
+    #re_path(r'^(?P<slug>[\w_-]+)/$', views.details, name='details'),
+]
