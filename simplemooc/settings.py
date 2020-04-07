873f738ffd63129bd0297633ec7aa75fa392fdf8
queirozsc <queirozsc@gmail.com>
2020-04-07 18:04:36 -0300

    Aula 31: Organizando o envio de e-mails com templates

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -124,3 +124,15 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
 MEDIA_URL = '/media/'
+
+# Emails
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' #https://myaccount.google.com/lesssecureapps?pli=1
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'Sergio Queiroz <queirozsc@gmail.com>'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'queirozsc@gmail.com'
+EMAIL_HOST_PASSWORD = '123456'
+EMAIL_PORT = 587
+
+CONTACT_EMAIL = 'queirozsc@gmail.com'
