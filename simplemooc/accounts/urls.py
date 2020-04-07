c8e0f355af017629a2c64783a3e66336732d7f53
queirozsc <queirozsc@gmail.com>
2020-04-07 20:31:40 -0300

    Aula 35: Form de cadastro

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, re_path
 from django.contrib.auth.views import LoginView
 
+from . import views
+
 urlpatterns = [
     path(
         'entrar/', 
         LoginView.as_view(template_name='accounts/login.html'),
         name='login'),
+    path(
+        'cadastre-se/', 
+        views.register,
+        name='register'),
 ]
