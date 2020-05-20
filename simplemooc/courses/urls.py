513cbe4b3a90f917adf832e4d8777a0aec101280
queirozsc <queirozsc@gmail.com>
2020-05-20 14:10:33 -0300

    Aula 53: Implementando a inscrição no curso

--- simplemooc/courses/urls.py
+++ simplemooc/courses/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<slug>', views.details, name='details'),
+    path('<slug>/inscricao', views.enrollment, name='enrollment'),
     # re_path(r'^(?P<pk>\d+)/$', views.details, name='details'),
     # re_path(r'^(?P<slug>[\w_-]+)/$', views.details, name='details'),
 ]
--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 from .forms import ContactCourse
-from .models import Course
+from .models import Course, Enrollment
 
 
 def index(request):
@@ -37,3 +38,11 @@ def details(request, slug):
     context['form'] = form
     context['course'] = course
     return render(request, template_name, context)
+
+@login_required
+def enrollment(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+    enrollment, created = Enrollment.objects.get_or_create(user=request.user, course=course)
+    if created:
+        enrollment.active()
+    return redirect('accounts:dashboard')
